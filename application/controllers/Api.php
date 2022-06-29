9171005e90417085028005088ccd6c566fbb786b
yusuf afandi <afandi.yusuf.04@gmail.com>
2022-06-29 19:45:24 +0700

    delete unnecessary space

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -477,22 +477,22 @@ class Api extends CI_Controller {
         $total = $this->M_notifications->get_notificaitons(null,$this->user->id,7);
         $string_notification = "";
         if($total > 0){
-            $string_notification .= "\n Ada $total panen kamu yang batal dijemput kurir!";
+            $string_notification .= "\nAda $total panen kamu yang batal dijemput kurir!";
         } 
 
         $total = $this->M_notifications->get_notificaitons(null,$this->user->id,6);
         if($total > 0){
-            $string_notification .= "\n Kurir sedang menjemput $total panen kamu!";
+            $string_notification .= "\nKurir sedang menjemput $total panen kamu!";
         }
 
         $total = $this->M_notifications->get_notificaitons(null,$this->user->id,8);
         if($total > 0){
-            $string_notification .= "\n Kadin sudah menugaskan kurir untuk menjemput $total panen kamu!";
+            $string_notification .= "\nKadin sudah menugaskan kurir untuk menjemput $total panen kamu!";
         }
 
         $total = $this->M_notifications->get_notificaitons(null,$this->user->id,5);
         if($total > 0){
-            $string_notification .= "\n Penjemputan sudah selesai, segera konfirmasi pengambilan!";
+            $string_notification .= "\nPenjemputan sudah selesai, segera konfirmasi pengambilan!";
         } 
 
         if($string_notification != ""){
